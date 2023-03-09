a214b8c023e9bc6197f1673b6de2bd389a01234e
Zaidann <96198627+zaidannn@users.noreply.github.com>
2023-03-09 08:25:05 +0700

    Add files via upload

---
+++ Hidangan.java
@@ -0,0 +1,15 @@
+public class Hidangan {
+    protected String namaHidangan;
+
+    public String getNamaHidangan() {
+        return namaHidangan;
+    }
+
+    public String setNamaHidangan(String namaHidangan) {
+        return namaHidangan = namaHidangan;
+    }
+
+    public String disantap() {
+        return "Makanan Dihidangkan";
+    }
+}
